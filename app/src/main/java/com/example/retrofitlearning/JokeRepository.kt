d4defe8d27aab53c986c25d8ea8221d726e93bbf
elsabagh <elsabagha025@gmail.com>
2023-04-12 02:02:25 +0200

    retrofit learning Mange Ui

--- app/src/main/java/com/example/retrofitlearning/JokeRepository.kt
+++ app/src/main/java/com/example/retrofitlearning/JokeRepository.kt
@@ -5,17 +5,27 @@ import com.example.retrofitlearning.model.State
 import com.example.retrofitlearning.networking.Api
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import retrofit2.Response
 
 class JokeRepository {
     suspend fun getRandomJoke(): Flow<State<JokeResponse?>> {
+        return WrapWithFlow(Api.apiService::getRandomJoke)
+    }
+
+    fun <T> WrapWithFlow(function: suspend () -> Response<T>): Flow<State<T?>> {
         return flow {
             emit(State.Loading)
-            val result = Api.apiService.getRandomJoke()
-            if (result.isSuccessful){
-                emit(State.Success(result.body()))
-            }else{
-                emit(State.Error(result.message()))
+            try {
+                val result = function()
+                if (result.isSuccessful) {
+                    emit(State.Success(result.body()))
+                } else {
+                    emit(State.Error(result.message()))
+                }
+            } catch (e: Exception){
+                emit(State.Error(e.message.toString()))
             }
+
         }
     }
 }
